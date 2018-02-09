d3f04bdda23fbe6f4cb4612a8f2aef135172dd74
David Lewis <davidlewisp@gmail.com>
2018-02-08 20:02:48 -0800

    write tthird spec to check for vowels

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -4,9 +4,19 @@ class Words
     @word2 = word2
   end
 
-  def check_letter
+  def check_letters
     first_word_array = @word1.downcase.split('').sort
     second_word_array = @word2.downcase.split('').sort
+
+    vowels = ["a", "e", "i", "o", "u", "y"]
+    has_vowel? == false
+    first_word_array.each do |letter|
+      vowels.each do |vowel|
+        if letter == vowel
+          has_vowel? == true
+        end
+    end
+
 
     if first_word_array == second_word_array
       "It's an anagram!"
--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -2,14 +2,19 @@ require('rspec')
 require('pry')
 require('anagrams_antigrams')
 
-describe('#check_letter') do
+describe('#check_letters') do
   it('Check if two words are anagrams') do
     word1 = Words.new("loop", "pool")
-    expect(word1.check_letter()).to(eq("It's an anagram!"))
+    expect(word1.check_letters()).to(eq("It's an anagram!"))
   end
 
   it('Check if two words are anagrams regardless of being upper or lower case') do
     word1 = Words.new("Loop", "pOOl")
-    expect(word1.check_letter()).to(eq("It's an anagram!"))
+    expect(word1.check_letters()).to(eq("It's an anagram!"))
+  end
+
+  it('Check if either word has vowels') do
+    word1 = Words.new("srnn", "Plpc")
+    expect(word1.check_letters()).to(eq("You did not enter an actual word silly!"))
   end
 end
