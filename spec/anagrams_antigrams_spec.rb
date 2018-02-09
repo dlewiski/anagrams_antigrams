98cbdf82202bdf107d657d7bfaf4f9b8fb1f9c94
David Lewis <davidlewisp@gmail.com>
2018-02-08 18:56:39 -0800

    Add first spec to determine if a word is an anagram

--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -1,3 +1,9 @@
 require('rspec')
 require('pry')
 require('anagrams_antigrams')
+
+describe('#anagram') do
+  to('Check if two words are anagrams') do
+    expect(anagram("loop", "pool")).to(eq("It's and anagram!"))
+  end
+end
