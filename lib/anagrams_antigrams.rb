13810fc76ddbc7a1e06d4e7afcbf2d66f4bc1503
David Lewis <davidlewisp@gmail.com>
2018-02-08 19:48:04 -0800

    Create second spec and get it to sucessfully pass

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -1,8 +1,12 @@
 class Words
-  def check_letter word1, word2
-    first_word_array = word1.split('').sort
-    second_word_array = word2.split('').sort
+  def initialize word1, word2
+    @word1 = word1
+    @word2 = word2
+  end
 
+  def check_letter
+    first_word_array = @word1.downcase.split('').sort
+    second_word_array = @word2.downcase.split('').sort
 
     if first_word_array == second_word_array
       "It's an anagram!"
--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -4,7 +4,12 @@ require('anagrams_antigrams')
 
 describe('#check_letter') do
   it('Check if two words are anagrams') do
-    word1 = Words.new
-    expect(word1.check_letter("loop", "pool")).to(eq("It's an anagram!"))
+    word1 = Words.new("loop", "pool")
+    expect(word1.check_letter()).to(eq("It's an anagram!"))
+  end
+
+  it('Check if two words are anagrams regardless of being upper or lower case') do
+    word1 = Words.new("Loop", "pOOl")
+    expect(word1.check_letter()).to(eq("It's an anagram!"))
   end
 end
